a98bb76a4357a8edc60877add1a2b0a7f991399c
HongLeongYong <32185642+HongLeongYong@users.noreply.github.com>
2021-02-23 17:42:46 +0800

    update 2.0
    finish level 5 - climbing ladder to the platform 2

---
+++ Test02/Assets/Scripts/LadderScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderScript : MonoBehaviour
+{
+    bool insideLadder;
+    public float ladderHeight = 3.3f;
+    public PlayerMovement playerMovement;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        insideLadder = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Ladder")
+        {
+            playerMovement.enabled = false;
+            insideLadder = !insideLadder;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            playerMovement.enabled = true;
+            insideLadder = !insideLadder;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (insideLadder && Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += Vector3.up / ladderHeight;
+        }
+    }
+}
